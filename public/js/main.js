926ee5643e40d45fc30601470c1022ceb7a76a13
Ariana Melis <arianamelis@gmail.com>
2019-04-14 01:14:41 -0700

    downloadable resume and handlebars set

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+// Wait until page is loaded
+$(document).ready(function() {
+    // Initialize Materialize components
+    $("select").formSelect();
+    $(".modal").modal();
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
+    $(".carousel").carousel();
+  });
+  
